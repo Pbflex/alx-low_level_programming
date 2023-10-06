9ae0bd23ae0c2e082fb0153a65352bf40b196800
Pbflex <bellopeter22291@gmail.com>
2023-10-05 21:30:03 -0700

    hello world task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is void
+ * Return: is always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
