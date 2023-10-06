9db18829054713ffd7a56793c1d13e62b69d0803
Pbflex <bellopeter22291@gmail.com>
2023-10-05 21:54:54 -0700

    size compiler

---
+++ 0x00-hello_world/6-sizes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function is void
+ * 
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char));
+	printf("size of an init: %lu byte(s)", sizeof(init));
+	print"size of a long init: %lu byte(s)", sizeof(long));
+	printf("size of a long long init: %lu byte(s)", sizeof(long long init));
+	printf("size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
