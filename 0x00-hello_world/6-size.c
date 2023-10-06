bdc54aa43b238067d39acd68dec7755c510b01f9
Pbflex <bellopeter22291@gmail.com>
2023-10-05 22:58:19 -0700

    Sizeof compilation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	printf("size of a char: %lu byte(s)", sizeof(char));
-	printf("size of an init: %lu byte(s)", sizeof(init));
-	print("size of a long init: %lu byte(s)", sizeof(long));
-	printf("size of a long long init: %lu byte(s)", sizeof(long long init));
+	printf("size of an int: %lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
