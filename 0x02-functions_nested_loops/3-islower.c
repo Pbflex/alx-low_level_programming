d8c2bd5e0be1314effea6418e63115b43b41c977
Pbflex <bellopeter22291@gmail.com>
2023-10-11 08:53:07 -0700

    update 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - islower is the function
  *
- *c - c is the int value
+ *@c: c is the int value
  * _putchar - use to print characters
  *  Write a function that checks for lowercase character
  *  Return: 1 if c is lowercase else 0
@@ -11,7 +11,7 @@
 
 int _islower(int c)
 {
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
