2e976620fb80cd092ead663a36fe7189f5ca7639
Pbflex <bellopeter22291@gmail.com>
2023-10-11 07:15:26 -0700

    void print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * _putchar - writes values
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
