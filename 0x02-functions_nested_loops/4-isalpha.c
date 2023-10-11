c098489ff3216f162afc75bc1204ee0b44e5d2cb
Pbflex <bellopeter22291@gmail.com>
2023-10-11 09:12:56 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - main function to detact alphabet
+ *
+ * @c: is to determine lowerand uppercase alphabet
+ * Task - Write a function that checks for alphabetic character
+ * Return: 1 if c is a letter or 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'z')
+		return (1);
+	else
+		return (0);
+}
