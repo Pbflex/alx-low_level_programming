250a9bc70b12845bab068125d8be7594c8a3633b
Pbflex <bellopeter22291@gmail.com>
2023-10-11 09:59:46 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @n: use to store value
+ * _putchar - use to print characters
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
