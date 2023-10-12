5db784f0298270ba4d6a9aecba854cd047beef36
Pbflex <bellopeter22291@gmail.com>
2023-10-12 08:43:56 -0700

    debugging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,18 @@ int main(void)
 {
 	int i;
 
-	/*printf("Infinite loop incoming :(\n");*/
+	("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
+	/**
+	 * while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 
-	/*printf("Infinite loop avoided! \\o/\n");*/
+	("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
