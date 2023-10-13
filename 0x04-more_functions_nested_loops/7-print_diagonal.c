43586c22acd9075141b58783286fc60ec93fd3a9
Pbflex <bellopeter22291@gmail.com>
2023-10-13 14:19:44 -0700

    7 print diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,19 @@ void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		while (n > 0)
+		int i, k;
+
+		for (i = 0; i < n; i++)
 		{
+			for (k = 0; k < i; k++)
+			_putchar(' ');
 			_putchar('\\');
-			n--;
 			_putchar('\n');
 		}
 	}
+	else
+	{
+
 	_putchar('\n');
+	}
 }
