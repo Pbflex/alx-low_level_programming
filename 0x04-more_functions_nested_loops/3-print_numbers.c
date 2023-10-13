0bf19acf892a1ca1d30ac8975ff4e37bf0583e01
Pbflex <bellopeter22291@gmail.com>
2023-10-12 18:50:37 -0700

    task 3 numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print numbers
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0'):
+	_putchar('\n');
+}
