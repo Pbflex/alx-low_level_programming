fe4a36bbf792a309a9d7a337c9b653b840792be7
Pbflex <bellopeter22291@gmail.com>
2023-10-17 16:01:16 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - main function
+ *
+ * @str: interger value
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int k;
+
+	for (k = 0 ; str[k] != '\0' ; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
