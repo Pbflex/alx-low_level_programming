d3b60e349de8a443395063019aaa1cba8a5c81f4
Pbflex <bellopeter22291@gmail.com>
2023-10-17 17:23:18 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - main function
+ *
+ * @dest: value 1
+ * @char: value 2
+ * @src: value 3
+ * Return: always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int k;
+
+	for (k = 0 ; src[k] != '\0'; k++)
+		dest[k] = src[k];
+	dest[k] = '\0';
+	return (dest);
+}
