bd56456f05e85921159a6bab33dc1d485fe7e03f
Pbflex <bellopeter22291@gmail.com>
2023-10-20 20:31:05 -0700

    task 11 recursive

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * infinite_add - main function
+ *
+ * @n1: value 1
+ * @n2: value 2
+ * @r: vale 3
+ * @r: value 4
+ * Return: always 0
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 9, tmp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 0)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		tmp_tot = val1 + val2 + overflow;
+		if (tmp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (tmp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	return (r);
+}
