a79da9afb0bd7fff8b9e1c2884bc95a7148f041e
Pbflex <bellopeter22291@gmail.com>
2023-10-20 18:30:44 -0700

    task 8 rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - main function used
+ *
+ * @s: character value
+ * Return: Always 0
+ */
+
+char *rot13(char *s)
+{
+	int k;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
+	char dataset[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		for (j = 0; j < 32; j++)
+		{
+			if (s[k] == data1[j])
+			{
+				s[k] = dataset[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
