3a4f8f284d736292a06982cfb1cb16bf3dd982ef
Pbflex <bellopeter22291@gmail.com>
2023-10-24 16:38:37 -0700

    Task 0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_memset - main function
+ *
+ * @s: first character
+ * @b: second character
+ * @n: third character
+ * Return: always 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[1] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#This is my even more ponters, arrays and string readme file
