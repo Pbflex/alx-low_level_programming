6b78385bb9e135bfda84b65c539eb72712bdf334
Pbflex <bellopeter22291@gmail.com>
2023-10-24 19:00:22 -0700

    Task 8 pointer to string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - main function
+ *
+ * @s: character value
+ * @to: character value 2
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
