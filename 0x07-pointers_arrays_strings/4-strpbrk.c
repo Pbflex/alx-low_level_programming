5c1d03b2f059d67e21e50e1b72ed639d1158dadb
Pbflex <bellopeter22291@gmail.com>
2023-10-24 17:46:06 -0700

    Task 4 strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - main function
+ *
+ * @s: first value
+ * @accept: second value
+ * Return: Always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int n;
+
+	while (*s)
+	{
+		for (n = 0; accept[n]; n++)
+		{
+			if (*s == accept[n])
+				return (s);
+		}
+		n++;
+	}
+	return ('\0');
+}
