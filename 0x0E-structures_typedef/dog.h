8a89546b3d03a63eae538a9d7597ee24bc141c5a
Pbflex <bellopeter22291@gmail.com>
2023-11-07 08:15:34 -0800

    Task 0 dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef POPPY_H
+#define POPPY_H
+/**
+ * struct dog - dog main value
+ *
+ * @name: first value
+ * @age: second value
+ * @owner: third value
+ * Return: Always 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
