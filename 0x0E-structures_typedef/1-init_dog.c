b0898d05f111a98b2b6571e97b96ceb4fcf7ef38
Pbflex <bellopeter22291@gmail.com>
2023-11-07 08:38:27 -0800

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - struct value
+ *
+ * @d: pointer to dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: always 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
